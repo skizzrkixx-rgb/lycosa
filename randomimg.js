364be8755dc6cdc185b635c6903271c6ee99861e
skizzrkixx-rgb <skizzrkixx@gmail.com>
2025-12-14 18:32:18 -0800

    Update randomimg.js

--- randomimg.js
+++ randomimg.js
@@ -1,7 +1,15 @@
 window.onload = choosePic;
 
 var myPix = new Array(
-
+"randomimg/alc.jpg",
+     "randomimg/beautifulsoul.jpg",
+     "randomimg/digitalfootprint.jpg",
+     "randomimg/evilandintimidating.jpg",
+     "randomimg/godgrantme.jpg",
+     "randomimg/map.jpg",
+     "randomimg/minesweeperhell.jpg",
+     "randomimg/situation.jpg",
+     "randomimg/webfunny.jpg"
 );
 
 function choosePic() {
@@ -9,4 +17,5 @@ function choosePic() {
      document.getElementById("myPicture").src = myPix[randomNum];
      setInterval(choosePic, 86400000); //- set time 2 generate (1 sec = 1000) (86400000 seconds = day)
 }
+
  // 
